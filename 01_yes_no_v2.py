cbf0d1959cdf10f33b5ceb444b16d4ee778027a4
Chisomo <107890763+chizzywizzy833@users.noreply.github.com>
2022-08-07 00:35:25 +1200

    more efficient yes no checker

--- 01_yes_no_v2.py
+++ 01_yes_no_v2.py
@@ -1,19 +1,17 @@
 display_instructions = ""
 while display_instructions.lower() != "xxx":
     # Ask the user if they have played before
-    show_instructions = input("Have you played this game before? ").lower()
+    display_instructions = input("Have you played this quiz before? ").lower()
 
     # If they say yes, output 'program continues'
     # If they say no, output 'display instructions'
     # If the answer is invalid, print an error
+
     if display_instructions == "yes" or display_instructions == "y":
-        display_instructions = "yes"
         print("program continues")
 
     elif display_instructions == "no" or display_instructions == "n":
-        display_instructions = "no"
-        print("display instruction")
+        print("display instructions")
 
-    # If they say anything else, output "please answer yes / no"
     else:
-        print("please answer either yes / no")
+        print("please answer either Yes / No")
