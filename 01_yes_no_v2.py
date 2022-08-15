5baaa3a72702b73f42300d18158af81189535eea
Chisomo <107890763+chizzywizzy833@users.noreply.github.com>
2022-08-16 09:06:39 +1200

    Yes and no checker with or statements (efficient)

--- 01_yes_no_v2.py
+++ 01_yes_no_v2.py
@@ -1,17 +1,24 @@
+"""
+yes_no checker component
+v1 - set up yes and checker
+v2 - make code more efficient using or statements
+"""
+
+# this is a loop that makes it easier for testing
 display_instructions = ""
 while display_instructions.lower() != "xxx":
+
     # Ask the user if they have played before
     display_instructions = input("Have you played this quiz before? ").lower()
 
     # If they say yes, output 'program continues'
-    # If they say no, output 'display instructions'
-    # If the answer is invalid, print an error
-
     if display_instructions == "yes" or display_instructions == "y":
         print("program continues")
 
+    # If they say no, output 'display instructions
     elif display_instructions == "no" or display_instructions == "n":
         print("display instructions")
 
+    # If the answer is invalid, print an error
     else:
         print("please answer either Yes / No")
