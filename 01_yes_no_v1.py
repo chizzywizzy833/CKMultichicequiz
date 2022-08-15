07834fe1728d74318ae5ad75202db4d7f0ce24b1
Chisomo <107890763+chizzywizzy833@users.noreply.github.com>
2022-08-16 09:01:38 +1200

    yes and no checker (trial)

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -1,24 +1,25 @@
 
-display_instructions = ""
-while display_instructions.lower() != "xxx":
+# this is a loop that makes it easier for testing
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+
     # Ask the user if they have played before
     show_instructions = input("Have you played this game before? ").lower()
 
     # If they say yes, output 'program continues'
+    if show_instructions == "yes":
+        print("program continues")
+
+    elif show_instructions == "y":
+        print("program continues")
+
     # If they say no, output 'display instructions'
-    # If the answer is invalid, print an error
-    if display_instructions == "yes":
-        print("program continues")
-
-    elif display_instructions == "y":
-        print("program continues")
-
-    elif display_instructions == "no":
+    elif show_instructions == "no":
         print("display instruction")
 
-    elif display_instructions == "n":
+    elif show_instructions == "n":
         print("display instructions")
 
     # If they say no, output "display instructions"
     else:
-        print("please answer either yes / no")
+        print("please answer yes / no")
